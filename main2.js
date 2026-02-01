3195710a44634b153f25e5c7608ff8e984b91fa5
Yash Mehta <yashm1562@gmail.com>
2026-02-01 12:49:40 +0530

    Update Valentine's message in main2.js

--- main2.js
+++ main2.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const wedate = document.getElementById("wedate");
         const btns = document.getElementById("btns");
         btns.style.display = "none";
-        wedate.innerText = "We are each other's valentine now. I love you cutie. ❤️😘";
+        wedate.innerText = ""I'm so lucky to have you. Happy Valentine's Day to my favorite CFA candidate! ❤️📈";
 
         // Open index1.html in a new tab
         window.open("index1.html", "_blank");
